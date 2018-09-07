0b414d2502ad18b2de7bffd5c3cfe5c9c8211f73
juan <dopplerjuan@gmail.com>
2018-09-07 17:05:23 -0300

    Control de rotacion segun distancia (discreto)
    Mide la distancia con un sensor ultrasonico y determina si es
    mayor a un valor umbral. Si es asi hace una rotacion completa.

---
+++ stepper_ultrasonic/stepper_ultrasonic.ino
@@ -0,0 +1,63 @@
+
+/*
+ Stepper Motor Control - one step at a time
+
+ This program drives a unipolar or bipolar stepper motor.
+ The motor is attached to digital pins 8 - 11 of the Arduino.
+
+ The motor will step one step at a time, very slowly.  You can use this to
+ test that you've got the four wires of your stepper wired to the correct
+ pins. If wired correctly, all steps should be in the same direction.
+
+ Use this also to count the number of steps per revolution of your motor,
+ if you don't know it.  Then plug that number into the oneRevolution
+ example to see if you got it right.
+
+ Created 30 Nov. 2009
+ by Tom Igoe
+
+ */
+
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+int stepCount = 0;         // number of steps the motor has taken
+
+// defines pins numbers
+const int trigPin = 13;
+const int echoPin = 12;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  // initialize the serial port:
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600);
+  myStepper.setSpeed(15);
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration*0.034/2;
+  // myStepper.step(1);
+  if(distance>10){
+    myStepper.step(stepsPerRevolution);
+  }
+}
